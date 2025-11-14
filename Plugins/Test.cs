1fc4e81df786683c7bba3be073668d139c5291d9
MelekTaus Moribundus <melektaus@outlook.at>
2025-11-14 22:45:08 +0100

    Upload test plugins

--- Plugins/Test.cs
+++ Plugins/Test.cs
@@ -1,5 +1,5 @@
-[Command("test", "Displays a test message", Parameters = "[name]")]
-public class Test : ICommand
+[Command("test1", "Displays a test message", Parameters = "[name]")]
+public class TestTest1 : ICommand
 {
     public async Task ExecuteAsync(string[] args, IServiceProvider services, CancellationToken cancellationToken = default)
     {
@@ -7,7 +7,7 @@ public class Test : ICommand
 
         var name = args.Length > 0 ? args[0] : "World";
 
-        logger?.Success($"Testing... {name}!");
+        logger?.Success($"Testing Nr. 1 ... {name}!");
         logger?.Info("This is a dynamically loaded plugin command!");
 
         await Task.CompletedTask;
--- Plugins/Test2.cs
+++ Plugins/Test2.cs
@@ -1,5 +1,5 @@
 [Command("test2", "Displays a test2 message", Parameters = "[name]")]
-public class Test2 : ICommand
+public class TestTest2 : ICommand
 {
     public async Task ExecuteAsync(string[] args, IServiceProvider services, CancellationToken cancellationToken = default)
     {
@@ -7,7 +7,7 @@ public class Test2 : ICommand
 
         var name = args.Length > 0 ? args[0] : "World";
 
-        logger?.Success($"Testing Nr. 2... {name}!");
+        logger?.Success($"Testing Nr. 2 ... {name}!");
         logger?.Info("This is a dynamically loaded plugin command!");
 
         await Task.CompletedTask;
